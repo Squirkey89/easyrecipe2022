7611844c9dc10f88dfaefbb6a2782ef71fc73908
Squirkey89 <squirke89@gmail.com>
2022-05-31 12:33:30 +0000

    Add recipe post and comment to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from . models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+
+class RecipeAdmin(SummernoteModelAdmin):
+  
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'recipe')
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('recipe', 'instructions')
+
+@admin.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
